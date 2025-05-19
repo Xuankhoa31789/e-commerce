f5358701dc932b2dda85096128f3fca16fdc5a23
pplugin <pluginldt@gmail.com>
2025-05-19 12:18:20 +0700

    Added OrderOperation

---
+++ src/main/java/com/xuka/op/OrderOperation.java
@@ -0,0 +1,16 @@
+package com.xuka.op;
+
+public class OrderOperation {
+    private static OrderOperation instance = null;
+
+    private OrderOperation() {
+        // Private constructor to prevent instantiation
+    }
+
+    public static OrderOperation getInstance() {
+        if (instance == null) {
+            instance = new OrderOperation();
+        }
+        return instance;
+    }
+}
